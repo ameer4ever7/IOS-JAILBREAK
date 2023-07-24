05a82f5e71b74fd8edc957a9ad4d89099825511c
Your Name <you@example.com>
2023-07-24 20:49:42 +0500

    new

---
+++ practice3 (6).py
@@ -0,0 +1,180 @@
+# # class lesson 1
+
+# class MyClass:
+  
+#   x = 5
+# p1 = MyClass()
+
+# print(p1.x)
+
+
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("Amir", 15)
+
+# print(p1.name)
+# print(p1.age)
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+# p1 = Person("Amir", 15)
+
+# print(p1)
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def __str__(self):
+#     return f"{self.name}({self.age})"
+
+# p1 = Person("Amir", 15)
+
+# print(p1)
+# class Person:
+#   def __init__(self, name, age):
+#     self.name = name
+#     self.age = age
+
+#   def myfunc(self):
+#     print("Hello my name is " + self.name)
+
+# p1 = Person("Amir", 15)
+# p1.myfunc()
+# class Person:
+#   def __init__(mysillyobject, name, age):
+#     mysillyobject.name = name
+#     mysillyobject.age = age
+
+#   def myfunc(abc):
+#     print("Hello my name is " + abc.name)
+
+# p1 = Person("Amir", 15)
+# p1.myfunc()
+# p1.age = 40
+# del p1.age
+# del p1
+# class Person:
+#   pass
+
+# class MyClass:
+#     son='Hello'
+#     mylist=[12, 45, 67, 53]
+#     mydict={"Anvar": 'Abdullayev', "brend": 'Qora'}
+
+# ob=MyClass()
+# print(ob.son)
+
+# class MyClass:
+#     son1='Qalay'
+#     xon=[12, 45, 68]
+
+#     def number_son(self):
+#         print(self.xon)
+
+# def set_xon(self, a):
+#     self.xon=a
+
+# my_object=MyClass()
+# my_object.set_son(23)
+# my_object.number_son()
+
+# class Myclass:
+#     son1='Hello'
+#     xon=(23, 56, "salom")
+#     def number_son(self):
+#         print(self.son1)
+  
+#     def set_son(self, new_son1):
+#         self.son1=new_son1
+
+# my_object=Myclass()
+# my_object.set_son(23)
+# my_object.number_son()
+
+# class Myclass:
+#     word=23
+#     mystr='hello'
+#     mylist=[34, 67, 90]
+
+#     def num_word(self):
+#         print(self.word)
+#         print(self.mystr)
+
+# my_object=Myclass()
+# print(my_object.word)
+# print(my_object.mystr)
+
+
+
+    # def __init__(self):
+    #     self.a = 2
+#     a=8
+        
+#     def output_a(self):
+#         print("Qiymati:", self.a)
+# obj = Oson1()
+# obj.output_a()
+
+# obj.a = 4
+# obj.output_a()
+
+# oson2
+#  class elon:
+#     a=7
+#     b=9
+#     def sum(self):
+#        return self.a + self.b
+#  my_elon = elon()
+#  my_elon.sum() # Output: 15
+
+class oson3:
+   a=int(input("Son Kiriting"))
+
+   def plus_minus(self):
+      if self.a > 0:
+         print("A Musbat")
+      elif self.a < 0:
+         print("B Manfiy")
+      else:
+        print("Musbat Manfiy")
+
+b=oson3()
+b.plus_minus()
+
+
+# # Example usage
+#         oson_object = oson3(5)
+#         oson_object.plus_minus()  
+# oson4
+# class oson4:
+#     def __init__(self, a):
+#         self.a = a
+    
+#     def odd_even(self):
+#         if (self.a % 2 == 0):
+#             print("The value of a is even")
+#         else:
+#             print("The value of a is odd")
+# my_object = oson4(5)
+# my_object.odd_even()
+# oson5
+# class oson5:
+#     def __init__(self, A, B):
+#         self.A = A
+#         self.B = B
+    
+#     def degree(self):
+#         result = self.B ** self.A
+#         print("{}th degree of {} is {}".format(self.A, self.B, result))
+# # create an instance of the class with A=2 and B=3
+# obj = oson5(2, 3)
+
+# # call the degree method
+# obj.degree()
+
